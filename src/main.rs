4b41d388ffae32caeb5f3be801e9bd6e45124974
Hannes <hannes-dev@mailbox.org>
2025-10-05 21:12:17 +0200

    use data dir for storage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::fs;
+use std::{fs, process::exit};
 
 use clap::Parser;
+use dirs_next::data_dir;
 
 use crate::{
     class::Class,
@@ -13,9 +14,25 @@ mod cli;
 mod commands;
 
 fn main() {
-    let classes_string = fs::read_to_string("classes.json").unwrap();
-    let classes_list: Vec<Class> = serde_json::from_str(&classes_string).unwrap();
-    let mut classes = Classes(classes_list);
+    // find and create the data directory
+    let path = match data_dir() {
+        None => exit(1),
+        Some(mut data_path) => {
+            data_path.push("lesson_tracker");
+            fs::create_dir_all(&data_path).unwrap();
+            data_path.push("classes.json");
+            data_path
+        }
+    };
+
+    // read classes if they exists, otherwise empty
+    let mut classes = if path.exists() {
+        let classes_string = fs::read_to_string(&path).unwrap();
+        let classes_list: Vec<Class> = serde_json::from_str(&classes_string).unwrap();
+        Classes(classes_list)
+    } else {
+        Classes(Vec::new())
+    };
 
     let cli = Cli::parse();
 
@@ -32,5 +49,5 @@ fn main() {
     }
 
     let json_string = serde_json::to_string_pretty(&classes.0).unwrap();
-    fs::write("classes.json", json_string).unwrap();
+    fs::write(&path, json_string).unwrap();
 }
